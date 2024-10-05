1fb4cc1ad895676a5615a705ba935a7d010435c2
Mihir123456JJ <kalothiamihir@gmail.com>
2024-10-05 18:23:54 +0530

    Tuple operations

---
+++ main.py
@@ -0,0 +1,17 @@
+#Write a program to perform the following operations:
+#1. Create a tuple of integers
+x=("Mihir",45,78.78,False)
+print(x)
+#2. Create another tuple of integers
+y=("Ney York","Long Island","Mumbai")
+print(y)
+#3. Create a new tuple by adding 9 to the previous tuple
+v=y+ (9,)
+print(v)
+#4. Count the occurrences of an element in a tuple
+q=(1,2,3,4,5,6,7,8,9,10,1,2,3234,12321,34212,111)
+print(q.count(1))
+#5. Perform slicing on tuple
+n=(1,12,23,444,565,65,3554,87675,56674,76,764,7)
+slice=n[2:4]
+print(slice)
